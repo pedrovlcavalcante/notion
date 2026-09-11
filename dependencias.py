49fb069232d428f13d0f38fc3351b034232f7a12
Pedro <pedrovitorl@gmail.com>
2026-09-11 18:06:09 -0300

    Avanços nas classes cliente e pedido

--- dependencias.py
+++ dependencias.py
@@ -1,21 +1,36 @@
-import psutil
+import psutil, json
 from bs4 import BeautifulSoup
 
-def selenium_esta_rodando():
-    # Nomes dos executáveis comuns de WebDrivers
-    drivers_selenium = ["chromedriver", "geckodriver", "msedgedriver"]
-    
-    for proc in psutil.process_iter(['name']):
-        try:
-            # Converte o nome para minúsculo para evitar problemas no Windows/Linux
-            nome_processo = proc.info['name'].lower()
-            
-            # Se encontrar o driver na lista de processos ativos
-            if any(driver in nome_processo for driver in drivers_selenium):
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            continue
-    return False
+
+estados_brasil = {
+    "Acre": "AC",
+    "Alagoas": "AL",
+    "Amapá": "AP",
+    "Amazonas": "AM",
+    "Bahia": "BA",
+    "Ceará": "CE",
+    "Distrito Federal": "DF",
+    "Espírito Santo": "ES",
+    "Goiás": "GO",
+    "Maranhão": "MA",
+    "Mato Grosso": "MT",
+    "Mato Grosso do Sul": "MS",
+    "Minas Gerais": "MG",
+    "Pará": "PA",
+    "Paraíba": "PB",
+    "Paraná": "PR",
+    "Pernambuco": "PE",
+    "Piauí": "PI",
+    "Rio de Janeiro": "RJ",
+    "Rio Grande do Norte": "RN",
+    "Rio Grande do Sul": "RS",
+    "Rondônia": "RO",
+    "Roraima": "RR",
+    "Santa Catarina": "SC",
+    "São Paulo": "SP",
+    "Sergipe": "SE",
+    "Tocantins": "TO"
+}
 
 regras_preco = {
     "CE": {
@@ -36,27 +51,139 @@ regras_preco = {
     },
 }
 
+def selenium_esta_rodando(): 
+    # Nomes dos executáveis comuns de WebDrivers
+    drivers_selenium = ["chromedriver", "geckodriver", "msedgedriver"]
+
+
+    for proc in psutil.process_iter(['name']):
+        try:
+            # Converte o nome para minúsculo para evitar problemas no Windows/Linux
+            nome_processo = proc.info['name'].lower()
+            
+            # Se encontrar o driver na lista de processos ativos
+            if any(driver in nome_processo for driver in drivers_selenium):
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            continue
+    return False
+
+def instancia_cliente(pedido):
+    with open(f"pedidos\\{pedido}.html", "rb") as html_content:
+        soup = BeautifulSoup(html_content, "lxml")
+    lista_cliente = soup.select_one("#selecionado_autocomplete_id_codigo_cliente > span > div > div:nth-child(1) > div:nth-child(1) > h5 > a").get_text(strip=True, separator="|").split("|")
+    lista_cnpj_cpf = soup.select_one("#selecionado_autocomplete_id_codigo_cliente > span > div > div:nth-child(1) > div:nth-child(1) > h5 > small:nth-child(3)").get_text(strip=True, separator=" ").split(" ")
+    destino = soup.select_one('#selecionado_autocomplete_id_codigo_cliente > span > div > div:nth-child(3) > div > span').text
+
+    # extrai cnpj ou cpf
+    if len(lista_cnpj_cpf) > 1:
+        fiscal = lista_cnpj_cpf[1]
+    else:
+        fiscal = lista_cnpj_cpf[0]
+    if len(lista_cliente) > 1:
+        nome_cliente = lista_cliente[1]
+    else:
+        nome_cliente = lista_cliente[0]
+    destino = destino.split(",")
+    cidade = destino[0].strip()
+    estado = destino[1].strip()
+    return Cliente(nome_cliente, estado, cidade, fiscal)
+
+def instancia_pedido(pedido, cliente):
+    # esse método coleta as informações do pedido continas na Mercos
+    with open(f"pedidos\\{pedido}.html", "rb") as html_content:
+        soup = BeautifulSoup(html_content, "lxml")
+    cod_integracao = soup.select_one("#js-div-global > div.overlay > section > div.container-fluid > div.box.bordered.padded > div.padded.acoes_pedido.barra-acao > script").text.strip().split()[2]
+    
+    # Informações extraídas da parte inferior da página
+    # 1ª coluna
+    num_pedido_mercos = soup.select_one("#informacoes_complementares > div > div > div:nth-child(1) > div:nth-child(1) > div > div.col-sm-8.label-valor").text
+    vendedor = soup.select_one('#informacoes_complementares > div > div > div:nth-child(1) > div:nth-child(4) > div > div.col-sm-8.label-valor').text
+    data_emissao = None
+
+    # 2ª coluna
+    parcela =  soup.select_one("#informacoes_complementares > div > div > div:nth-child(2) > div:nth-child(1) > div > div.js-condicao-pagamento.col-sm-8.label-valor").text
+    outros_campos = soup.select_one("#informacoes_complementares > div > div > div:nth-child(2)")
+
+    #3ª coluna
+    transportadora = soup.select_one("#informacoes_complementares > div > div > div:nth-child(3) > div:nth-child(2) > div > div.col-sm-8.label-valor").text
+
+
+    # essa variável se encontra no fim dessa div, quase no rodapé
+    info_adicionais = soup.select_one("#informacoes_complementares > div > div > div.flex.col-sm-12.tpadded20 > div.label-valor").text
+
+    # loop para encontrar a porcentagem da nota
+    for child in outros_campos.find_all():
+        texto_limpo = child.get_text(strip=True)
+        if texto_limpo == "* TABELA DE ENVIO DE MERCADORIA":
+            # .find_next_sibling() pega o elemento irmão que vem logo depois dele
+            proximo_elemento = child.find_next_sibling()
+            if proximo_elemento:
+                porcentagem = proximo_elemento.get_text(strip=True)
+                break
+
+    # loop para encontar a tabela de preço           
+    for child in outros_campos.find_all():
+        texto_limpo = child.get_text(strip=True)
+        if texto_limpo == "* TABELA USADA NO PEDIDO":
+            # .find_next_sibling() pega o elemento irmão que vem logo depois dele
+            texto_tabela = child.find_next_sibling()
+            if texto_tabela:
+                tabela = texto_tabela.get_text(strip=True)
+                
+                break
+
+    # loop para encontrar a observação interna
+    for child in outros_campos.find_all():
+        texto_limpo = child.get_text(strip=True)
+        if texto_limpo == "* Observação Interna":
+            # .find_next_sibling() pega o elemento irmão que vem logo depois dele
+            texto_observacao = child.find_next_sibling()
+            if texto_observacao:
+                observacao = texto_observacao.get_text(strip=True)
+                
+                break
+    return Pedido(num_pedido_mercos, tabela, porcentagem, cliente, None)
+
 class Cliente():
     def __init__(self, nome, estado, cidade, fiscal):
         self.nome = nome
-        self.fiscal = fiscal
         self.estado = estado
         self.cidade = cidade
+        self.fiscal = fiscal
+        self.uf = estados_brasil.get(estado)
+        self.codigo_integracao = self.pega_codigo_integracao()
+
+    def pega_codigo_integracao(self):
+        with open("clientes.json", "rb") as file:
+            clientes = json.load(file)
+        try:
+            codigo_cliente = clientes[self.fiscal]['codigo']
+        except KeyError as e:
+            print("Cliente não encontrado, insira o codigo manualmente...")
+            codigo_cliente = input("Código cliente: ")
+        return codigo_cliente
 
 class Pedido():
-    def __init__(self, numero, tabela_mercos, valor_pedido, cliente:Cliente):
+    def __init__(self, numero, tabela_mercos, porcentagem, cliente:Cliente, produtos):
         self.numero = numero
+        self.itens = None
+        self.quantidade = None
         self.tabela_mercos = tabela_mercos
-        self.valor_pedido = valor_pedido
+        self.porcentagem = porcentagem
+        self.produtos = produtos
+        self.valor_pedido = None
         self.cliente = cliente
         self.frete_tabelado = self.calcula_frete_tabelado()
         self.codigo_integração = None
 
     def calcula_frete_tabelado(self):
-
         estado = self.cliente.estado
         tabela_mercos = self.tabela_mercos
         valor_pedido = self.valor_pedido
+        #apagar esse if depois, apenas para testes
+        if valor_pedido == None:
+            valor_pedido = 2000
         # Agrupamentos de estados
         nordeste = ["AL", "BA", "MA", "PB", "PE", "PI", "RN", "SE"]
         norte = ["AC", "AP", "AM", "PA", "RO", "RR", "TO"]
@@ -101,68 +228,9 @@ class Pedido():
                 return preco
         return 0  
 
-    def dados_pedido(self, soup):
-        # esse método coleta as informações do pedido continas na Mercos
-        
-        # Informções extraídas da parte superior da página
-        # lista_cliente e lista_cnpj_cpf podem ser listas ou não, a depender se o icone aparece antes do nome
-        cod_integracao = soup.select_one("#js-div-global > div.overlay > section > div.container-fluid > div.box.bordered.padded > div.padded.acoes_pedido.barra-acao > script").text.strip().split()[2]
-        lista_cliente = soup.select_one("#selecionado_autocomplete_id_codigo_cliente > span > div > div:nth-child(1) > div:nth-child(1) > h5 > a").get_text(strip=True, separator="|").split("|")
-        lista_cnpj_cpf = soup.select_one("#selecionado_autocomplete_id_codigo_cliente > span > div > div:nth-child(1) > div:nth-child(1) > h5 > small:nth-child(3)").get_text(strip=True, separator=" ").split(" ")
-        destino = soup.select_one('#selecionado_autocomplete_id_codigo_cliente > span > div > div:nth-child(3) > div > span').text
-
-        # Informações extraídas da parte inferior da página
-        # 1ª coluna
-        num_pedido_mercos = soup.select_one("#informacoes_complementares > div > div > div:nth-child(1) > div:nth-child(1) > div > div.col-sm-8.label-valor").text
-        vendedor = soup.select_one('#informacoes_complementares > div > div > div:nth-child(1) > div:nth-child(4) > div > div.col-sm-8.label-valor').text
-
-        # 2ª coluna
-        parcela =  soup.select_one("#informacoes_complementares > div > div > div:nth-child(2) > div:nth-child(1) > div > div.js-condicao-pagamento.col-sm-8.label-valor").text
-        porcentagem = soup.select_one("#informacoes_complementares > div > div > div:nth-child(2)")
-        info_adicionais = soup.select_one("#informacoes_complementares > div > div > div.flex.col-sm-12.tpadded20 > div.label-valor").text
-
-        #3ª coluna
-        transportadora = soup.select_one("#informacoes_complementares > div > div > div:nth-child(3) > div:nth-child(2) > div > div.col-sm-8.label-valor").text
-
-        # extrai cnpj ou cpf
-        if len(lista_cnpj_cpf) > 1:
-            cnpj_cpf = lista_cnpj_cpf[1]
-        else:
-            cnpj_cpf = lista_cnpj_cpf[0]
-        if len(lista_cliente) > 1:
-            cliente = lista_cliente[1]
-        else:
-            cliente = lista_cliente[0]
-
-        for child in porcentagem.find_all():
-            texto_limpo = child.get_text(strip=True)
-            if texto_limpo == "* TABELA DE ENVIO DE MERCADORIA":
-                # .find_next_sibling() pega o elemento irmão que vem logo depois dele
-                proximo_elemento = child.find_next_sibling()
-                if proximo_elemento:
-                    pct = proximo_elemento.get_text(strip=True)
-                    break
-
-        for child in porcentagem.find_all():
-            texto_limpo = child.get_text(strip=True)
-            if texto_limpo == "* TABELA USADA NO PEDIDO":
-                # .find_next_sibling() pega o elemento irmão que vem logo depois dele
-                texto_tabela = child.find_next_sibling()
-                if texto_tabela:
-                    tabela = texto_tabela.get_text(strip=True)
-                    
-                    break
-
-        for child in porcentagem.find_all():
-            texto_limpo = child.get_text(strip=True)
-            if texto_limpo == "* Observação Interna":
-                # .find_next_sibling() pega o elemento irmão que vem logo depois dele
-                texto_observacao = child.find_next_sibling()
-                if texto_observacao:
-                    observacao = texto_observacao.get_text(strip=True)
-                    
-                    break
-        texto_completo = f"INFORMAÇÕES PEDIDO: {pedido}"            
+    def imprime_informações(self):
+        # Tenho q puxar a tabela e calcular o valor do pedido aqui valor_pedido
+        texto_completo = f"INFORMAÇÕES PEDIDO: {self.numero}"            
         pedido_formatado = f"R$ {valor_pedido:,.2f}"
         
         print("=" * 50)
@@ -187,11 +255,15 @@ class Pedido():
 
 if __name__=="__main__":
 
-    cliente = Cliente("Pedro", "SP", "SP")
-    pedido = Pedido(12345, "10", 4700, cliente=cliente)
-    print(pedido.numero)
+    cliente = instancia_cliente(14886)
+    print(cliente.nome)
+    print(cliente.cidade)
+    print(cliente.estado)
+    print(cliente.uf)
+    print(cliente.fiscal)
+    print(cliente.codigo_integracao)
+
+    pedido = instancia_pedido(14886, cliente)
+    print(pedido.valor_pedido)
+    print(pedido.tabela_mercos)
     print(pedido.frete_tabelado)
-    print(pedido.cliente.nome)
-    # tabela = pedido.preco_tabelado("10", "MA", 3700)
-    # print(tabela)
-    
